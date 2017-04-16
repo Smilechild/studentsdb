9a18e11771a9e9e784b560b88ce8cbcd36a90ffe
Nazarii Onishchuk <nazar.smiluk@gmail.com>
2017-04-16 20:25:53 +0300

    base.html sample made and some changes in students_list.html and groups_list.html

--- students/views.py
+++ students/views.py
@@ -38,7 +38,18 @@ def students_delete(request, sid):
 # Views for Groups
 
 def groups_list(request):
-	return HttpResponse('<h1>Groups Listing</h1>')
+	groups=(
+		{'id': 1,
+		'group_name': u'МтМ-21',
+		'group_leader': u'Ячменев Олег',},
+		{'id': 2,
+		'group_name': u'МтМ-22',
+		'group_leader': u'Віталій Подоба',},
+		{'id': 3,
+		'group_name': u'МтМ-23',
+		'group_leader': u'Іванов Андрій',},
+	)
+	return render(request, 'students/groups_list.html',{'groups': groups})
 
 def groups_add(request):
 	return HttpResponse('<h1>Groups Add Form</h1>')
--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -29,9 +29,9 @@ urlpatterns = [
 
     # Groups urls
     url(r'^groups/$', 'students.views.groups_list', name='groups'),
-    url(r'^groups/add/$', 'students/views/groups_add', name='groups_add'),
-    url(r'^groups/(?P<gid>\d+)/edit/$', 'students/views/gtoups_edit', name='groups_edit'),
-    url(r'^groups/(?P<gid>\d+)/delete/$', 'students/views/gtoups_delete', name='groups_delete'),
+    url(r'^groups/add/$', 'students.views.groups_add', name='groups_add'),
+    url(r'^groups/(?P<gid>\d+)/edit/$', 'students.views.groups_edit', name='groups_edit'),
+    url(r'^groups/(?P<gid>\d+)/delete/$', 'students.views.groups_delete', name='groups_delete'),
 
 
     url(r'^admin/', admin.site.urls),
