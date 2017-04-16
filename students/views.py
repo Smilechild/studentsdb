606c8f5f266bc7a6e056006e7163064ac43b2a5f
Nazarii Onishchuk <nazar.smiluk@gmail.com>
2017-04-16 16:14:29 +0300

    students list in views and for loop in students_list

--- students/views.py
+++ students/views.py
@@ -7,7 +7,24 @@ from django.http import HttpResponse
 # Views for Students
 
 def students_list(request):
-	return render(request, 'students/students_list.html',{})
+	students =(
+		{'id': 1,
+		'first_name': u'Андрій',
+		'last_name': u'Корост',
+		'ticket': 2123,
+		'image': 'img/01.jpg'},
+		{'id': 2,
+		'first_name': u'Віталій',
+		'last_name': u'Подоба',
+		'ticket': 254,
+		'image': 'img/02.jpg'},
+		{'id': 3,
+		'first_name': u'Тарас',
+		'last_name': u'Притула',
+		'ticket': 5332,
+		'image': 'img/03.jpg'},
+	)
+	return render(request, 'students/students_list.html',{'students': students})
 
 def students_add(request):
 	return HttpResponse('<h1>Student Add Form</h1>')
